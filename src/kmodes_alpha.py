962ed08342edc49b5100c1a68d175e821df496d5
Thomas Townsley <thomastownsley@localhost.localdomain>
2019-11-19 15:16:06 -0600

    reduced for loop complexity to 1 loop

--- src/kmodes_alpha.py
+++ src/kmodes_alpha.py
@@ -16,33 +16,40 @@ df = pd.read_csv(file, encoding='utf-8', header=None)
 df = df.drop(df.columns[[0]], axis=1)
 df.index+=1
 
-#Outfile testwrite 
+#File MSA Output 
 outf = open('outfile.txt', 'w')
+outf.writelines('\nMultiple Sequence Alignment\n' + df.to_string(index = False) + '\n'*2)
 
 n = len(df.columns)
 k = n
 
-#outf.writelines('\nMultiple Sequence Alignment\n' + df.to_string(index = False) + '\n'*2)
+#Initialization 
+k -= 1
+k_random_samples = df.sample(k, axis = 1)
+cluster_list = [pd.DataFrame(k_random_samples[i]) for i in k_random_samples]
+remaining_attr = df[df.columns.difference(k_random_samples.columns, False)]
+max_rii = 0 
+    
+for location, cluster in enumerate(cluster_list):
+    cluster_mode = cluster[cluster.columns[0]] 
+    rii = nmis(remaining_attr[remaining_attr.columns[0]], cluster_mode, average_method='arithmetic')
+    if rii > max_rii: 
+        max_rii, best_cluster = rii, location       
+cluster_list[best_cluster] = cluster_list[best_cluster].join(remaining_attr)
 
-while (k > 2): 
-    k -= 1
-    k_random_samples = df.sample(k, axis = 1)
-    cluster_list = [pd.DataFrame(k_random_samples[i]) for i in k_random_samples]
-    remaining_attr = df[df.columns.difference(k_random_samples.columns, False)]
+#Write out initial pairwise clustering
+outf.writelines('First Pairwise Association at K = ' + str(k) + '\n')
+for cluster in cluster_list:
+    outf.writelines(str(cluster.columns.values).replace('[','(').replace(']',')'))
 
-    for i in remaining_attr:
-        max_rii = 0 
-        for location, cluster in enumerate(cluster_list):  
-            for j in cluster:
-                rii = nmis(remaining_attr[i], cluster[j], average_method='arithmetic')
-            if rii > max_rii: 
-                max_rii, best_cluster = rii, location         
-        cluster_list[best_cluster] = cluster_list[best_cluster].join(remaining_attr[i])
 
+
+'''
     outf.writelines('\nValue of K:' + str(k) + '\n')
     for cluster in cluster_list:
          outf.writelines(str(cluster.columns.values).replace('[','(').replace(']',')'))
     outf.writelines('\n'*2)
+'''
 
 outf.close()
 spinner.stop()
