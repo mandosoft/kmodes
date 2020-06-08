fe54f8bfdeb2737d6d5c6a2714ac63260af71b10
mandosoft <thomasdtownsley@gmail.com>
2020-06-08 14:17:07 -0500

    Formatted changes

--- src/kmodes_alpha_h.py
+++ src/kmodes_alpha_h.py
@@ -9,18 +9,17 @@ from src.gui import *
 warnings.simplefilter(action='ignore', category=FutureWarning)
 sys.stdout.write("\nLooking for strong pairwise associations...")
 
-# Cluster Initialization
-df = submit_and_run.df  # df passed from gui.py
+df = submit_and_run.df
 cluster_list = [pd.DataFrame(df[i]) for i in df]
 cluster_list_clean = cluster_list.copy()
 each_2nd_col = df[df.columns[::2]]
 
-# Compare each second column to whole list. This finds pairwise associations quickly.
+"""Compare each second column to whole list. This finds pairwise associations quickly."""
 for i in each_2nd_col:
     max_rii = 0
     rii = 0
     for location, cluster in enumerate(cluster_list):
-        cluster_mode = cluster[cluster.columns[0]]  # helps distinguish as Series object
+        cluster_mode = cluster[cluster.columns[0]]
         if each_2nd_col[i].name != cluster_mode.name:
             rii = nmis(each_2nd_col[i], cluster_mode, average_method='arithmetic')
             if rii > max_rii:
@@ -28,9 +27,8 @@ for i in each_2nd_col:
     # noinspection PyUnboundLocalVariable
     cluster_list[best_cluster] = pd.concat([cluster_list[best_cluster], each_2nd_col[i]], axis=1)
 
-# --------Pre-processing for input into k-modes algorithm-----------------------
-# All list processing below prevents shared attributes between pairwise associations
 
+"""All list processing below prevents shared attributes between pairwise associations"""
 pairwise = 2
 pairwise_list = [cluster for cluster in cluster_list if len(cluster.columns) == pairwise]
 pairwise_cluster_labels = [list(cluster.columns.values) for cluster in pairwise_list]
@@ -43,15 +41,13 @@ for each in pairwise_cluster_labels:
         del_list = list(filter((each[1]).__ne__, del_list))
         unique_list.append(each)
 
-# Compare clusters to list of unique values
 unique_pairwise_clusters = list()
 for cluster in pairwise_list:
     for i in unique_list:
         if (cluster.columns.values == i).all():
-            unique_pairwise_clusters.append(cluster)  # list of dataframes
+            unique_pairwise_clusters.append(cluster)
 
-# We now have all unique pairwise clusters.
-# Get remaining data as single attributes
+"""Get remaining data as single attributes"""
 series_from_upc = [i for cluster in unique_pairwise_clusters for i in cluster]
 series_clean_list = [j for cluster in cluster_list_clean for j in cluster]
 final_set = [item for item in series_clean_list if item not in series_from_upc]
@@ -60,16 +56,12 @@ difference_set = list()
 for i in cluster_list_clean:
     for j in final_set:
         if j == i.columns.values:
-            difference_set.append(pd.DataFrame(i))  # loads single attributes into their own dataframes
+            difference_set.append(pd.DataFrame(i))
 
 unique_pairwise_clusters.extend(difference_set)
 
-# -------------------------------------------------------------------------------
-
-# Hard copy cluster list and ready for K modes algorithm
+"""Main vars for K Modes Iterations"""
 cluster_list = unique_pairwise_clusters.copy()
-
-# Main globals. Use wisely.
 k = len(cluster_list)
 max_rii = 0
 rii = 0
@@ -97,6 +89,7 @@ def calculate_sr_mode(c: pd.DataFrame) -> pd.DataFrame:
 def calculate_new_mode(c: pd.DataFrame) -> pd.DataFrame:
 
     max_sum = 0
+    # TODO specify dtype explicitly
     cluster_mode = pd.Series()
     for location, i in enumerate(c):
         sum_rii = 0
@@ -118,22 +111,20 @@ def calculate_new_mode(c: pd.DataFrame) -> pd.DataFrame:
     return c
 
 
-# --------------- K Modes Algorithm --------------------
-
-# Store pairwise associations in dictionary
+"""--------------- K Modes Algorithm --------------------"""
 cluster_list = [calculate_sr_mode(cluster) for cluster in cluster_list]
 
 while k != 2:
 
     k -= 1
-    sys.stdout.write("\n***Running K modes at iteration #:" + str(k))
+    sys.stdout.write("\n--> Running K modes at iteration #:" + str(k))
 
-    # Begin by selecting a random mode from list
+    """Begin by selecting a random mode from list"""
     enumerated_list = list(enumerate(cluster_list))
     random_df_il, random_df = random.choice(enumerated_list)
     new_mode = random_df[random_df.columns[0]]
 
-    # Add random mode to best cluster
+    """Add random mode to best cluster"""
     rii = 0
     max_rii = 0
     for location, cluster in enumerated_list:
@@ -145,10 +136,10 @@ while k != 2:
     cluster_list[random_df_il] = cluster_list[random_df_il].drop(cluster_list[random_df_il].columns[0], axis=1)
     cluster_list[best_cluster] = pd.concat([cluster_list[best_cluster], new_mode], axis=1)
 
-    # Calculate the new mode for the best cluster
+    """Calculate the new mode for the best cluster"""
     cluster_list[best_cluster] = calculate_new_mode(cluster_list[best_cluster])
 
-    # Go back to the cluster the random mode came from and auction off remaining attributes
+    """Go back to the cluster the random mode came from and auction off remaining attributes"""
     if cluster_list[random_df_il].empty:
         del cluster_list[random_df_il]
 
@@ -169,7 +160,7 @@ while k != 2:
     else:
         sys.stderr.write('\nThere was a problem breaking up a cluster.')
 
-    # Compute new mode and sr mode for each cluster in the cluster list
+    """Compute new mode and sr mode for each cluster in the cluster list"""
     cluster_list = [calculate_new_mode(cluster) for cluster in cluster_list]
     cluster_list = [calculate_sr_mode(cluster) for cluster in cluster_list]
 
