a61d9e102d8bea81a569971f6c1250d27f783809
mandosoft <thomasdtownsley@gmail.com>
2020-07-16 11:20:00 -0500

    Shell installation script added

--- kmodes.sh
+++ kmodes.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
+# Shell installer/launcher for macOS Catalina
 
 set -e # abort on error
-# To my loving spacewitch
-# shellcheck disable=SC2164
-cd src/kmodes_alpha
-echo "compiling kmodes to executable"
-pyinstaller --onefile \
---name kmodes_app \
---specpath ../../ \
--p kmodes_alpha \
---windowed \
---console \
-main.py
 
-cd dist
-./kmodes_app
+DIR="kmodes"
+
+cd ~/Desktop
+
+if [ -d $DIR ]
+then
+    echo "{$DIR} exists."
+else
+    xcode-select --install || softwareupdate --install -a && softwareupdate --restart
+    GIT=git
+
+    yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    brew install python
+    brew update && brew upgrade python
+    brew install graphviz
+
+    echo "Checking git"
+    if [ ! -f $GIT ]; then
+        echo "### Git installation failed"
+        exit 255
+    fi
+
+    $GIT clone https://github.com/mandosoft/kmodes.git
+    cd kmodes
+    pip3 install -r requirements.txt
+fi
+
+git clean -fd
+git pull origin master
+
+python3 main.py
+
